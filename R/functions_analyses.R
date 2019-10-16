92430aadb50030e40a599dc48d784d3f70ca15eb
Coye <C.Coye@exeter.ac.uk>
2019-10-16 14:21:27 +0100

    Here we changed the last argument of the function (i.e. the ,...), we see what has changed, i.e. modified stuff in red, new version in green.

--- R/functions_analyses.R
+++ R/functions_analyses.R
@@ -1,5 +1,5 @@
 file_read <- function () {
-  read.csv(fileName,header=TRUE, stringsAsFactors = FALSE,...)
+  read.csv(fileName,header=TRUE, stringsAsFactors = FALSE)
 }
 
 # cette fonction permet d'ouvrir les CSV sans transformer automatiquement les strings comme des facteurs
